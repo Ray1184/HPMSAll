013e216ac56317b73cf4623557ebdf493604ebf4
Ray1184 <nickgreppi1184@gmail.com>
2022-09-04 11:36:18 +0200

    Actor input manager

--- modules/Main/Runtime/resources/scripts/libs/logic/managers/PlayerActionsManager.lua
+++ modules/Main/Runtime/resources/scripts/libs/logic/managers/PlayerActionsManager.lua
@@ -19,18 +19,19 @@ function player_actions_manager:new(player, roomState, sceneMgr)
     k = game_mechanics_consts:get()
     local this = {
         module_name = 'player_actions_manager',
-        player = player,
-        room_state = roomState,
         interactive = true,
         input_prf = input_profile:new(context_get_input_profile()),
-        scene_mgr = sceneMgr,
         actions =
         {
             walk = 0,
+            walkRatio = 0,
             rotate = 0,
-            action_1 = false,
-            action_2 = false,
-            action_3 = false
+            action_1_once = false,
+            action_2_once = false,
+            action_3_once = false,
+            action_1_doing = false,
+            action_2_doing = false,
+            action_3_doing = false
         }
     }
     log_debug('Creating player actions manager module')
@@ -40,86 +41,141 @@ function player_actions_manager:new(player, roomState, sceneMgr)
         return insp.inspect(o)
     end
 
-    function player_actions_manager:poll_inputs(keys, mouseButtons)
-        local scene = self.scene_mgr:get_scene_name()
+    function player_actions_manager:poll_inputs(keys, mouseButtons, scene)
+        self.input_prf:poll_inputs(keys, mouseButtons)
         if keys ~= nil and self.interactive then
 
-            if lib.key_action_performed(keys, k.input_actions.ESC, 1) then
+            if self.input_prf:action_done_once(k.input_actions.EXIT) then
                 scene.quit = true
                 return
             end
 
-            if lib.key_action_performed(keys, k.input_actions.UP, 2) then
+            if self.input_prf:action_doing(k.input_actions.UP) then
                 self.actions.walk = 1
-            elseif lib.key_action_performed(keys, k.input_actions.DOWN, 2) then
+            elseif self.input_prf:action_doing(k.input_actions.DOWN) then
                 self.actions.walk = -1
             else
                 self.actions.walk = 0
             end
-            if lib.key_action_performed(keys, k.input_actions.RIGHT, 2) then
+            if self.input_prf:action_doing(k.input_actions.RIGHT) then
                 self.actions.rotate = -1
-            elseif lib.key_action_performed(keys, k.input_actions.LEFT, 2) then
+            elseif self.input_prf:action_doing(k.input_actions.LEFT) then
                 self.actions.rotate = 1
             else
                 self.actions.rotate = 0
             end
 
-            if lib.key_action_performed(keys, k.input_actions.ACTION_1, 1) then
-                self.actions.action_1 = true
+            if self.input_prf:action_done_once(k.input_actions.ACTION_1) then
+                self.actions.action_1_once = true
             else
-                self.actions.action_1 = false
+                self.actions.action_1_once = false
             end
 
-            if lib.key_action_performed(keys, k.input_actions.ACTION_2, 1) then
-                self.actions.action_2 = true
+            if self.input_prf:action_doing(k.input_actions.ACTION_1) then
+                self.actions.action_1_doing = true
             else
-                self.actions.action_2 = false
+                self.actions.action_1_doing = false
             end
 
-            if lib.key_action_performed(keys, k.input_actions.ACTION_3, 1) then
-                self.actions.action_3 = true
+            if self.input_prf:action_done_once(k.input_actions.ACTION_2) then
+                self.actions.action_2_once = true
             else
-                self.actions.action_3 = false
+                self.actions.action_2_once = false
             end
+
+            if self.input_prf:action_doing(k.input_actions.ACTION_2) then
+                self.actions.action_2_doing = true
+            else
+                self.actions.action_2_doing = false
+            end
+
+            if self.input_prf:action_done_once(k.input_actions.ACTION_3) then
+                self.actions.action_3_once = true
+            else
+                self.actions.action_3_once = false
+            end
+
+            if self.input_prf:action_doing(k.input_actions.ACTION_3) then
+                self.actions.action_3_doing = true
+            else
+                self.actions.action_3_doing = false
+            end
+
+
 
         end
     end
 
     function player_actions_manager:update(tpf)
-        local walkRatio = 0
         if self.actions.walk == 1 then
-            walkRatio = walkRatio + tpf * 10
-            if walkRatio > 1 then
-                walkRatio = 1
+            self.actions.walkRatio = self.actions.walkRatio + tpf * 10
+            if self.actions.walkRatio > 1 then
+                self.actions.walkRatio = 1
             end
         elseif self.actions.walk == -1 then
-            walkRatio = walkRatio - tpf * 10
-            if walkRatio < -1 then
-                walkRatio = -1
+            self.actions.walkRatio = self.actions.walkRatio - tpf * 10
+            if self.actions.walkRatio < -1 then
+                self.actions.walkRatio = -1
             end
         else
-            if walkRatio > 0 then
-                walkRatio = walkRatio - tpf * 10
-                if walkRatio < 0 then
-                    walkRatio = 0
+            if self.actions.walkRatio > 0 then
+                self.actions.walkRatio = self.actions.walkRatio - tpf * 10
+                if self.actions.walkRatio < 0 then
+                    self.actions.walkRatio = 0
                 end
             else
-                walkRatio = walkRatio + tpf * 10
-                if walkRatio > 0 then
-                    walkRatio = 0
+                self.actions.walkRatio = self.actions.walkRatio + tpf * 10
+                if self.actions.walkRatio > 0 then
+                    self.actions.walkRatio = 0
                 end
             end
 
         end
         local turn = self.actions.rotate ~= 0
-        local walkF = walkRatio > 0
-        local walkB = walkRatio < 0
+        local walkF = self.actions.walk > 0
+        local walkB = self.actions.walk < 0
         player.serializable.performing_action = false
-        if interactive then
-            if action then
-                player:set_anim(k.default_animations.PUSH)
-                player:play(k.anim_modes.ANIM_MODE_LOOP, 1)
+        if self.interactive then
+            if self.actions.action_2_doing then
                 player.serializable.performing_action = true
+
+                local cbks = { }
+
+                cbks[k.actor_action_mode.SEARCH] = function()
+
+                end
+
+                cbks[k.actor_action_mode.COMBAT] = function()
+
+                end
+
+                cbks[k.actor_action_mode.EQUIP] = function()
+
+                end
+
+                cbks[k.actor_action_mode.PUSH] = function()
+                    player:set_anim(k.default_animations.PUSH)
+                    player:play(k.anim_modes.ANIM_MODE_LOOP, 1)
+                end
+
+                cbks[k.actor_action_mode.STEALTH] = function()
+
+                end
+
+                cbks[k.actor_action_mode.SWIM] = function()
+
+                end
+
+                cbks[k.actor_action_mode.JUMP] = function()
+
+                end
+
+                cbks[k.actor_action_mode.CROWL] = function()
+
+                end
+
+                cbks[player.serializable.action_mode]()
+
             elseif walkF or(walkF and turn) then
                 player:set_anim(k.default_animations.WALK_FORWARD)
                 player:play(k.anim_modes.ANIM_MODE_LOOP, 1)
@@ -134,27 +190,16 @@ function player_actions_manager:new(player, roomState, sceneMgr)
                 player:play(k.anim_modes.ANIM_MODE_LOOP, 2, 1)
             end
         end
-        if not action then
+        if not self.actions.action_2_doing then
             player:rotate(0, 0, 150 * tpf * self.actions.rotate)
         end
-        player:move_dir(tpf * walkRatio * 5)
+        player:move_dir(tpf * self.actions.walkRatio)
     end
 
     function player_actions_manager:set_interactive(flag)
         self.interactive = flag
     end
 
-    function player_actions_manager:is_action_1()
-        return self.actions.action_1
-    end
-
-    function player_actions_manager:is_action_2()
-        return self.actions.action_2
-    end
-
-    function player_actions_manager:is_action_3()
-        return self.actions.action_3
-    end
 
     return this
 end
